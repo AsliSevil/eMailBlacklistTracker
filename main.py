bae6b09e25d51bfc08b9268cdfa21f8f56be44fd
Asli Sevil Ozcan <aslisevil@hotmail.com>
2020-10-28 14:31:34 +0300

    Fixed Main

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import serviceControll
 import blacklistSearch
 # -------------------------------------------------------------------
 # IP YA DA DOMAIN:
-ip = 'google.com'
+input_ = 'google.com'
 
 
 #ULTRATOOLS
-search_blacklist = blacklistSearch.UltraTools(ip)
+search_blacklist = blacklistSearch.UltraTools(input_)
 search_blacklist.searchForBlacklist()
 
 # -------------------------------------------------------------------
 #MXTOOLBOX
-search_blacklist = blacklistSearch.MxToolbox(ip)
+search_blacklist = blacklistSearch.MxToolbox(input_)
 search_blacklist.searchForBlacklist()
 
 
 
 #WHAT IS MY IP
-search_blacklist = blacklistSearch.WhatIsMyIp(ip)
+search_blacklist = blacklistSearch.WhatIsMyIp(input_)
 search_blacklist.searchForBlacklist()
 
 # -------------------------------------------------------------------
@@ -33,12 +33,12 @@ LinesDnsbl = fileDnsbl.readlines()
 
 for line in LinesDnsbl:
     params_dnsblInfo = {'bl': line,
-                        'ip': ip,
+                        'ip': input_,
                         'cb': '1047725336',
                         'token': '19f7a69caad8e97c7c1198883edd0938'
                         }
     url_dnsblInfo = 'https://www4.dnsbl.info/test-a.php?'
-    dnsblInfo = serviceControll.Service(url_dnsblInfo, params_dnsblInfo, ip)
+    dnsblInfo = serviceControll.Service(url_dnsblInfo, params_dnsblInfo, input_)
     dnsblInfo.searchService()
 # -------------------------------------------------------------------
 
